ec30b885ad76aa6665f4b78c87e86a6c4505b0a4
Paul <mandackpaplo@gmail.com>
2026-02-20 09:23:37 +0100

    Change DodoPayments environment to live_mode

--- lib/dodo.ts
+++ lib/dodo.ts
@@ -6,5 +6,5 @@ if (!process.env.DODO_PAYMENTS_API_KEY) {
 
 export const dodo = new DodoPayments({
     bearerToken: process.env.DODO_PAYMENTS_API_KEY || "",
-    environment: "test_mode",
+    environment: "live_mode",
 });
